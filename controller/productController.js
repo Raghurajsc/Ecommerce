8aa0a9328c720884d61d2feb198e157617a05eb3
raghuraj <raghurajsc562@gmail.com>
2026-06-05 13:32:50 +0530

    Added HTML view for products endpoint

--- controller/productController.js
+++ controller/productController.js
@@ -1,8 +1,8 @@
+const path=require("path");
 const productService = require("../services/productService");
 
 const getAllProducts = (req, res) => {
-    const result = productService.getAllProducts();
-    res.send(result);
+    res.sendFile(path.join(__dirname,"../views/products.html"));
 };
 
 const addProduct = (req, res) => {
