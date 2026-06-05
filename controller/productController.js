8b8e0dd6ff503e8160385f9cf6f0d63d90085255
raghuraj <raghurajsc562@gmail.com>
2026-06-05 13:14:49 +0530

    Added product service layer

--- controller/productController.js
+++ controller/productController.js
@@ -1,17 +1,22 @@
-const getAllProducts=(req,res)=>{
-res.send("Fetching all products");
+const productService = require("../services/productService");
+
+const getAllProducts = (req, res) => {
+    const result = productService.getAllProducts();
+    res.send(result);
 };
 
-const addProduct=(req,res)=>{
-    res.send("Adding a new product");
+const addProduct = (req, res) => {
+    const result = productService.addProduct();
+    res.send(result);
 };
 
-const getProductById=(req,res)=>{
-    res.send(`Fetching products with ID: ${req.params.id}`);
+const getProductById = (req, res) => {
+    const result = productService.getProductById(req.params.id);
+    res.send(result);
 };
 
-module.exports={
+module.exports = {
     getAllProducts,
     addProduct,
     getProductById
-}
+};
